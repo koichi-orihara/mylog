5a29a778b6147bedd40acd723a48a6cbe0482054
koichi.orihara <koichi_orihara@gdo.co.jp>
2022-09-01 00:29:44 +0000

    createFromFunction

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+//フォルダ作成機能
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
