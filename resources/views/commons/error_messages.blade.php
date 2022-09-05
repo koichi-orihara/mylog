f204b642b0c1da5bcec0ed7661f2d22c9e4ee115
koichi.orihara <koichi_orihara@gdo.co.jp>
2022-09-05 00:32:09 +0000

    login register

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,8 +1,8 @@
 @if (count($errors) > 0)
 
-    <ul class="alert alert-danger" role="alert">
+    <div class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
-            <li class="ml-4">{{ $error }}</li>
+            <div class="ml-4">{{ $error }}</div>
         @endforeach
-    </ul>
+    </div>
 @endif
